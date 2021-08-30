2db0da8d0049aa7d0c2e4a8592d60b26057b2a9b
RachanaAnand93 <rachanaa03@gmail.com>
2021-08-30 14:07:33 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
-      <p>
-        <a
-          href="https://github.com/RachanaAnand93/Weather-App"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>
-        , by Rachana Anand
-      </p>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <p>
+          <a
+            href="https://github.com/RachanaAnand93/Weather-App"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>
+          , by Rachana Anand
+        </p>
+      </div>
     </div>
   );
 }
