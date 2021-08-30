eab92a12bb34d7e4ad9ba11ada73590e4dbdabfd
RachanaAnand93 <rachanaa03@gmail.com>
2021-08-30 20:03:31 +0200

    added HTML and CSS to project

--- src/Weather.js
+++ src/Weather.js
@@ -1,56 +1,48 @@
 import React, { useState } from "react";
+import "./weather.css";
 import axios from "axios";
 
 export default function Weather() {
-  let [city, setCity] = useState("");
-  let [display, setDisplay] = useState(false);
-  let [details, setDetails] = useState("");
-
-  function changeCity(event) {
-    setCity(event.target.value);
-  }
-  function getDetails(response) {
-    //console.log(response.data);
-    setDisplay(true);
-    setDetails({
-      temp: response.data.list[0].main.temp,
-      wind: response.data.list[0].wind.speed,
-      humidity: response.data.list[0].main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.list[0].weather[0].icon}@2x.png`,
-      description: response.data.list[0].weather[0].description,
-    });
-  }
-  function handleSubmit(event) {
-    event.preventDefault();
-    if (city.length === 0) {
-      alert("Enter a location");
-    } else {
-      let apiKey = "56db1fa98457edda6eb57bb6a4699df0";
-      let url = `https://api.openweathermap.org/data/2.5/find?q=${city}
-    &appid=${apiKey}&units=metric`;
-      axios.get(url).then(getDetails);
-    }
-  }
-  let form = (
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder=" Enter city name" onChange={changeCity} />
-      <input type="submit" value="Search" />
-    </form>
-  );
-  if (display) {
-    return (
-      <div className="weather">
-        {form}
-        <li>Temperature: {Math.round(details.temp)}°C </li>
-        <li>Description: {details.description} </li>
-        <li>Humidity: {details.humidity}% </li>
-        <li>Wind: {details.wind}km/h </li>
-        <li>
-          <img src={details.icon} alt={details.description} />
-        </li>
+  return (
+    <div className="Weather">
+      <form>
+        <div className="container">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a location.."
+                className="Search-city"
+              />
+            </div>
+            <div className="col-3">
+              <button className="btn btn-primary p-2"> Search</button>
+            </div>
+          </div>
+        </div>
+      </form>
+      <h1>Berlin</h1>
+      <ul className="Dates-description">
+        <li> Wednesday 07:10</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="Row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+            rel="noreferrer"
+            alt="cloudy"
+          />
+          <div className="Temper"> 12°C|°F</div>
+        </div>
+        <div className="col-6">
+          <ul className="Details">
+            <li>Precipitation:91%</li>
+            <li>Humidity:87%</li>
+            <li>Wind:16km/h</li>
+          </ul>
+        </div>
       </div>
-    );
-  }
-
-  return <div className="App">{form}</div>;
+    </div>
+  );
 }
